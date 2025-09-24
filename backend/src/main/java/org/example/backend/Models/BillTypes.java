b402e9c9a11bed2620072b411a62169b31e7a78e
Tomás Valero <tomasvalero998@gmail.com>
2025-09-24 18:46:22 -0300

    Creadas las entidades principales de la aplicación

---
+++ backend/src/main/java/org/example/backend/Models/BillTypes.java
@@ -0,0 +1,27 @@
+package org.example.backend.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bill_types")
+public class BillTypes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private String typeName;
+
+    @OneToMany(mappedBy = "billType")
+    private java.util.List<Bill> bills;
+}
